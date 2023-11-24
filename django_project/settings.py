23b8aa4c140c1c54b7dae7e910accd69c9f9e643
Okemwag <gabrielokemwa83@gmail.com>
2023-11-24 21:21:29 +0300

    Initial Commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -70,25 +70,26 @@ TEMPLATES = [
     },
 ]
 
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+"""
 
-# For Docker/PostgreSQL usage uncomment this and comment the DATABASES config above
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "postgres",
-#         "HOST": "db",  # set in docker-compose.yml
-#         "PORT": 5432,  # default postgres port
-#     }
-# }
+
+DATABASES = {
+     "default": {
+         "ENGINE": "django.db.backends.postgresql",
+         "NAME": "postgres",
+         "USER": "postgres",
+         "PASSWORD": "postgres",
+         "HOST": "db",  
+         "PORT": 5432,  
+     }
+ }
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
